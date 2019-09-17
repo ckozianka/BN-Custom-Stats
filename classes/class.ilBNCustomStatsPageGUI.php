4dc3ae4c5d444f942cfe6457387caaa99ac6ba54
ckozianka <kozianka@uni-bonn.de>
2019-09-17 14:39:34 +0200

    Added ilLocator to BNCustomStatsPageGUI.php

--- classes/class.ilBNCustomStatsPageGUI.php
+++ classes/class.ilBNCustomStatsPageGUI.php
@@ -2,6 +2,7 @@
 
  /*
   *  @ilCtrl_isCalledBy ilBNCustomStatsPageGUI: ilUIPluginRouterGUI
+  *  @ilCtrl_Calls ilBNCustomStatsPageGUI: ilObjUserTrackingGUI, ilObjSystemFolderGui
   */
 
 class ilBNCustomStatsPageGUI
@@ -9,12 +10,14 @@ class ilBNCustomStatsPageGUI
     
     function __construct() 
     {
-        global /*$ilCtrl,*/ $tpl;
-        //$this->ctrl = $ilCtrl;
+        global $ilCtrl, $tpl, $ilTabs, $ilLocator;
+        $this->ctrl = $ilCtrl;
         $this->tpl = $tpl;
+        $this->tabs = $ilTabs;
+        $this->locator = $ilLocator;
         $this->plugin = ilPlugin::getPluginObject(IL_COMP_SERVICE, 'UIComponent', 'uihk', 'BNCustomStats');
     }
-    
+
     function executeCommand ()
     {
         $this->index();
@@ -22,12 +25,35 @@ class ilBNCustomStatsPageGUI
     
     function index()
     {
+        //Get standard template
         $this->tpl->getStandardTemplate();
+        
+        //Set locator/breadcrumbs
+        $this->ctrl->setParameterByClass("ilObjSystemFolderGUI", "ref_id", 9);
+        $this->locator->addItem("Administration", $this->ctrl->getLinkTargetByClass(array('ilAdministrationGUI','ilObjSystemFolderGUI')));
+        $this->ctrl->setParameterByClass("ilObjUserTrackingGUI", "ref_id", 17);
+        $this->locator->addItem("Statistics and Learning Progress", $this->ctrl->getLinkTargetByClass(array('ilAdministrationGUI','ilObjUserTrackingGUI'), 'view'));
+        $this->locator->addItem("eCampus-Statistik", $this->ctrl->getLinkTargetByClass(array('ilUIPluginRouterGUI', 'ilBNCustomStatsPageGUI'),'index'));
+        $this->tpl->setLocator();
+        
+        //Set page title
         $this->tpl->setTitle("eCampus-Statistik");
+        
+        //Set page icon
         $title_image = $this->plugin->getImagePath("ecampus_logo.svg");
         $this->tpl->setTitleIcon($title_image);
+        
+        //Set page description
         $this->tpl->setDescription("Spezielle Auswertungen für das eCampus-Team");
+        
+        //Tabs
+        //$this->tabs->addTab("test", "eCampus-Statistik", $this->ctrl->getLinkTargetByClass(array('ilUIPluginRouterGUI', 'ilBNCustomStatsPageGUI')));
+        //$this->tabs->activateTab("test");
+        
+        //Set output to main content
         $this->tpl->setContent("Hallo Welt!");
+        
+        //Render template
         $this->tpl->show();
     }
     
